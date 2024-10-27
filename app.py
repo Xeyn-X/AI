c4378212b56d34898e42b2604c4ec3a960d7d813
Hein Htet Arkar Mg <57092161+Xeyn-X@users.noreply.github.com>
2024-10-27 18:56:45 +0630

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def get_base64_image(file_path):
     return encoded
 
 # Path to your local image file
-image_path = "./resources/img/bg-1.jpg"
+image_path = "./resources/img/bg.jpg"
 img_base64 = get_base64_image(image_path)
 st.markdown(
     f"""
